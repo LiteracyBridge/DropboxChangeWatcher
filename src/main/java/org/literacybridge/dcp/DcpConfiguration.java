d7b39db9b4f220bc48f1bd5f87a6c547558e22ed
Jeff Lubetkin <jefflub@gmail.com>
2015-03-22 13:21:38 -0700

    More config stuff

--- src/main/java/org/literacybridge/dcp/DcpConfiguration.java
+++ src/main/java/org/literacybridge/dcp/DcpConfiguration.java
@@ -1,7 +1,39 @@
 package org.literacybridge.dcp;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
- * Created by jefflub on 3/22/15.
+ * Configuration values
  */
 public class DcpConfiguration {
+
+    private static final int DEFAULT_DROPBOX_POLL_TIMEOUT=30;
+    private static final int DEFAULT_CONNECTION_READ_TIMEOUT=125;
+
+    Properties properties;
+
+    public DcpConfiguration( String propertiesFile ) throws IOException {
+        properties = new Properties();
+        properties.load( new FileReader( propertiesFile ) );
+        if ( getAppKey() == null || getAppSecret() == null )
+            throw new IOException( "AppKey or AppSecret is missing from properties file." );
+    }
+
+    public String getAppKey() {
+        return properties.getProperty( "dropbox-app-key" );
+    }
+
+    public String getAppSecret() {
+        return properties.getProperty( "dropbox-app-secret" );
+    }
+
+    public String getAccessToken() {
+        return properties.getProperty( "dropbox-access-token" );
+    }
+
+    public int getPollTimeout() {
+        return Integer.getInteger(properties.getProperty("dropbox-poll-timeout"), DEFAULT_DROPBOX_POLL_TIMEOUT);
+    }
 }
--- src/main/java/org/literacybridge/dcp/DropboxChangeProcessor.java
+++ src/main/java/org/literacybridge/dcp/DropboxChangeProcessor.java
@@ -13,7 +13,6 @@ import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Locale;
 
 /**
@@ -21,51 +20,74 @@ import java.util.Locale;
  */
 public class DropboxChangeProcessor {
 
-    private static final class MyHttpRequestor extends StandardHttpRequestor
-    {
+    /**
+     * The standard HttpRequestor in the Dropbox API times out after 35 seconds, but longpoll reads can be as long as
+     * the longpoll timeout PLUS 90 seconds. Make the read timeout at least longpoll timeout, plus 90, plus a bonus
+     * 5 for good luck.
+     */
+    private static final class MyHttpRequestor extends StandardHttpRequestor {
+        private int readTimeoutSeconds;
+
+        private MyHttpRequestor(int longpollTimeout) {
+            this.readTimeoutSeconds = longpollTimeout + 95;
+        }
+
         @Override
         protected void configureConnection(HttpsURLConnection conn) throws IOException {
-            super.configureConnection( conn );
-            conn.setReadTimeout( 125 * 1000 );
+            super.configureConnection(conn);
+            conn.setReadTimeout(readTimeoutSeconds * 1000);
         }
     }
 
-    public static void main( String[] args ) throws IOException, DbxException, InterruptedException {
-        final String APP_KEY = "dcmewl4bae8dhhh";
-        final String APP_SECRET = "jbryotuhmlo5txs";
+    public static void main(String[] args) throws IOException, DbxException, InterruptedException {
 
-        DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
+        if (args.length != 1) {
+            System.out.println("Usage: DropboxChangeProcessor <properties file>");
+            System.exit(-1);
+        }
 
-        DbxRequestConfig config = new DbxRequestConfig(
-                "LiteracyBridge DBChangeProcessor/1.0", Locale.getDefault().toString(), new MyHttpRequestor());
-        DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
+        DcpConfiguration dcpConfig = null;
+        try {
+            dcpConfig = new DcpConfiguration(args[0]);
+        } catch (IOException ex) {
+            System.out.println("Failed to load properties file " + args[0]);
+            ex.printStackTrace();
+            System.exit(-1);
+        }
 
-        // Have the user sign in and authorize your app.
-        /*
-        String authorizeUrl = webAuth.start();
-        System.out.println("1. Go to: " + authorizeUrl);
-        System.out.println("2. Click \"Allow\" (you might have to log in first)");
-        System.out.println("3. Copy the authorization code.");
-        String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
-        DbxAuthFinish authFinish = webAuth.finish( code );
-        */
+        DbxAppInfo appInfo = new DbxAppInfo(dcpConfig.getAppKey(), dcpConfig.getAppSecret());
 
-        String accessToken = "nO4h8oh1-9QAAAAAAAAB8TBCboxaaDBxolzEf1rt_zNw0Ee3X_b08WYwDUFR4pBC";
-        DbxClient client = new DbxClient( config, accessToken );
+        DbxRequestConfig config = new DbxRequestConfig("LiteracyBridge DBChangeProcessor/1.0",
+                Locale.getDefault().toString(), new MyHttpRequestor(dcpConfig.getPollTimeout()));
+
+        if (dcpConfig.getAccessToken() == null) {
+            DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
+            String authorizeUrl = webAuth.start();
+            System.out.println("1. Go to: " + authorizeUrl);
+            System.out.println("2. Click \"Allow\" (you might have to log in first)");
+            System.out.print("3. Copy the authorization code and enter here: ");
+            String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+            DbxAuthFinish authFinish = webAuth.finish(code);
+            System.out.println("Access token is '" + authFinish.accessToken + "'");
+            System.out.println("Add this to the properties file as a property named 'dropbox-access-token'.");
+            System.exit(0);
+        }
+
+        String accessToken = dcpConfig.getAccessToken();
+        DbxClient client = new DbxClient(config, accessToken);
+
         System.out.println("Linked account: " + client.getAccountInfo().displayName);
 
-        String cursor = getLatestDeltaCursor( config, accessToken );
-        System.out.println( "Latest Delta Cursor: " + cursor );
+        String cursor = getLatestDeltaCursor(config, accessToken);
+        System.out.println("Latest Delta Cursor: " + cursor);
 
-        while ( true )
-        {
-            LongpollResponse longpollResponse = pollForChanges( config, accessToken, cursor );
-            System.out.println( "Response! Changes: " + longpollResponse.changes + " Backoff: " + longpollResponse.backoff );
-            if ( longpollResponse.changes )
-            {
-                cursor = processChanges( client, cursor );
+        while (true) {
+            LongpollResponse longpollResponse = pollForChanges(config, accessToken, cursor);
+            System.out.println("Response! Changes: " + longpollResponse.changes + " Backoff: " + longpollResponse.backoff);
+            if (longpollResponse.changes) {
+                cursor = processChanges(client, cursor);
             }
-            Thread.sleep( longpollResponse.backoff * 1000 );
+            Thread.sleep(longpollResponse.backoff * 1000);
         }
     }
 
@@ -83,12 +105,10 @@ public class DropboxChangeProcessor {
                 try {
                     if (fieldName.equals("cursor")) {
                         cursorId = JsonReader.StringReader.readField(parser, fieldName, cursorId);
-                    }
-                    else {
+                    } else {
                         JsonReader.skipValue(parser);
                     }
-                }
-                catch (JsonReadException ex) {
+                } catch (JsonReadException ex) {
                     throw ex.addFieldContext(fieldName);
                 }
             }
@@ -101,7 +121,7 @@ public class DropboxChangeProcessor {
         }
     };
 
-    private static String getLatestDeltaCursor( DbxRequestConfig config, String accessToken ) throws DbxException {
+    private static String getLatestDeltaCursor(DbxRequestConfig config, String accessToken) throws DbxException {
         String[] params = {
                 "cursor", null,
                 "path_prefix", null,
@@ -112,24 +132,21 @@ public class DropboxChangeProcessor {
                     @Override
                     public String handle(HttpRequestor.Response response) throws DbxException {
                         if (response.statusCode != 200) throw DbxRequestUtil.unexpectedStatus(response);
-                        return DbxRequestUtil.readJsonFromResponse( CursorReader, response.body);
+                        return DbxRequestUtil.readJsonFromResponse(CursorReader, response.body);
                     }
                 });
     }
 
-    private static class LongpollResponse
-    {
+    private static class LongpollResponse {
         public boolean changes;
         public long backoff;
 
-        public LongpollResponse( boolean changes, long backoff )
-        {
+        public LongpollResponse(boolean changes, long backoff) {
             this.changes = changes;
             this.backoff = backoff;
         }
 
-        public static final class Reader extends JsonReader<LongpollResponse>
-        {
+        public static final class Reader extends JsonReader<LongpollResponse> {
             @Override
             public LongpollResponse read(JsonParser parser) throws IOException, JsonReadException {
                 JsonLocation top = JsonReader.expectObjectStart(parser);
@@ -144,27 +161,24 @@ public class DropboxChangeProcessor {
                     try {
                         if (fieldName.equals("changes")) {
                             changes = JsonReader.BooleanReader.readField(parser, fieldName, null);
-                        }
-                        else if (fieldName.equals("backoff")) {
-                            backoff = JsonReader.readUnsignedLongField( parser, fieldName, 0);
-                        }
-                        else {
+                        } else if (fieldName.equals("backoff")) {
+                            backoff = JsonReader.readUnsignedLongField(parser, fieldName, 0);
+                        } else {
                             JsonReader.skipValue(parser);
                         }
-                    }
-                    catch (JsonReadException ex) {
+                    } catch (JsonReadException ex) {
                         throw ex.addFieldContext(fieldName);
                     }
                 }
 
                 JsonReader.expectObjectEnd(parser);
 
-                return new LongpollResponse( changes, backoff );
+                return new LongpollResponse(changes, backoff);
             }
         }
     }
 
-    private static LongpollResponse pollForChanges( DbxRequestConfig config, String accessToken, String cursor ) throws DbxException {
+    private static LongpollResponse pollForChanges(DbxRequestConfig config, String accessToken, String cursor) throws DbxException {
         String[] params = {
                 "cursor", cursor,
                 "timeout", "30"
@@ -180,7 +194,7 @@ public class DropboxChangeProcessor {
                 });
     }
 
-    private static final String processChanges( DbxClient client, String cursor ) throws DbxException {
+    private static final String processChanges(DbxClient client, String cursor) throws DbxException {
         DbxDelta<DbxEntry> delta = null;
         do {
             delta = client.getDelta(cursor);
@@ -192,7 +206,7 @@ public class DropboxChangeProcessor {
                     System.out.println("File added/changed");
             }
             cursor = delta.cursor;
-        } while ( delta.hasMore );
+        } while (delta.hasMore);
 
         return delta.cursor;
     }
